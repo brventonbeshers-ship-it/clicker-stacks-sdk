530dc90c508d316c3a569f5fab6dd410b4beed48
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-13 03:19:46 +0300

    ci: add growth pulse workflow for SDK downloads

---
+++ growth-bot/index.js
@@ -0,0 +1,5 @@
+import * as sdk from 'clicker-stacks-sdk';
+
+console.log('Growth bot running for clicker-stacks-sdk...');
+console.log('SDK exports:', Object.keys(sdk).join(', '));
+console.log('Smoke check passed.');
