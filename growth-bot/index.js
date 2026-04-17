c0ddc088725e7319dac108ad1ecf7b4805d33e48
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-17 13:18:28 +0300

    chore: clean up index.js

--- growth-bot/index.js
+++
@@ -1,5 +0,0 @@
-import * as sdk from 'clicker-stacks-sdk';
-
-console.log('Growth bot running for clicker-stacks-sdk...');
-console.log('SDK exports:', Object.keys(sdk).join(', '));
-console.log('Smoke check passed.');
